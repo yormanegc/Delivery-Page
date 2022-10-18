7df9241023a02d72b4235745f0174d1058c6fbc9
Yorman Gonzalez <yormanegcr@gmail.com>
2022-10-18 10:41:43 -0400

    the project is added

---
+++ tailwind.config.js
@@ -0,0 +1,50 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+const { yellow } = require('tailwindcss/colors');
+const { black } = require('tailwindcss/colors');
+
+
+module.exports = {
+    purge: [
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+    ],
+
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    variants: {
+        extend: {
+            opacity: ['disabled'],
+        },
+    },
+
+    plugins: [require('@tailwindcss/forms'), require('@tailwindcss/typography')],
+
+    
+       
+
+};
+
+module.exports = {
+    
+    theme : {
+        colors{
+            transparent: 'transparent',
+            current: 'currentColor',
+            black: colors.black,
+            white: colors.white,
+            gray: colors.trueGray,
+            indigo: colors.indigo,
+            red: colors.rose,
+            yellow: colors.amber,
+        }
+    }
+}
+
